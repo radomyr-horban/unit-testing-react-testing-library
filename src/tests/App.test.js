d12c1f91b03ed52c1d6644d027919e8529f77382
Andrii Kuz <andrykuz4556@gmail.com>
2022-12-13 00:22:33 +0200

    Task1 - 'Write tests for App component' - is done (CORRECTED)

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -2,7 +2,7 @@ import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import App from '../components/App';
 
-xdescribe('App component', () => {
+describe('App component', () => {
   test('Should be displayed image when the first tab is selected', () => {
     render(<App />);
     const pictureBtn = screen.getByRole('button', { name: 'Picture' });
