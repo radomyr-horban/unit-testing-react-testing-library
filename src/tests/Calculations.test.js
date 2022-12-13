68e0590b7b66c6664b367a687423aba83a1d4902
Andrii Kuz <andrykuz4556@gmail.com>
2022-12-13 22:03:14 +0200

    Changed Calculations tests

--- src/tests/Calculations.test.js
+++ src/tests/Calculations.test.js
@@ -22,4 +22,22 @@ describe('Calculations component', () => {
     const result = screen.getByRole('heading', { level: 4 });
     expect(result).toHaveTextContent('3');
   });
+
+  test('Check if evaluation of the result is correct with "additional" option', () => {
+    render(<Calculations />);
+
+    const firstInput = screen.getByRole('spinbutton', { name: 'first number' });
+    userEvent.type(firstInput, '8');
+
+    const secondInput = screen.getByRole('spinbutton', {
+      name: 'second number',
+    });
+    userEvent.type(secondInput, '2');
+
+    const evaluateBtn = screen.getByText('Evaluate');
+    userEvent.click(evaluateBtn);
+
+    const result = screen.getByRole('heading', { level: 4 });
+    expect(result).toHaveTextContent('10');
+  });
 });
